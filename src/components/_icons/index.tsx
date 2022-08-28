91071ef8248c1b427eb245638e91d3ee02120614
JHSeo <qnfqnfqnf@gmail.com>
2022-08-28 22:36:47 +0900

    feat: add admonition icons

--- src/components/_icons/index.tsx
+++ src/components/_icons/index.tsx
@@ -1,2 +1,7 @@
+export { default as FireIcon } from './Fire';
+export { default as ImportantIcon } from './Important';
+export { default as InfoIcon } from './Info';
+export { default as LightBulbIcon } from './LightBulb';
 export { default as LogoIcon } from './Logo';
 export { default as TextLogoIcon } from './TextLogo';
+export { default as WarningFillIcon } from './WarningFill';
