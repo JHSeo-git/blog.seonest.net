c07462772630a301072a918e3961107556b97936
JHSeo <qnfqnfqnf@gmail.com>
2022-09-06 03:21:18 +0900

    fix: add dependency in intersectionObserver effect

--- src/components/PostNav/useHeadingInViewEffect.ts
+++ src/components/PostNav/useHeadingInViewEffect.ts
@@ -1,9 +1,11 @@
+import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
 import { useHeadingView } from './HeadingViewProvider';
 
 export default function useHeadingInViewEffect(id: string) {
   const { setHeadingId } = useHeadingView();
+  const { asPath } = useRouter();
 
   useEffect(() => {
     if (typeof window === 'undefined') {
@@ -30,5 +32,5 @@ export default function useHeadingInViewEffect(id: string) {
     return () => {
       observer.disconnect();
     };
-  }, []);
+  }, [asPath]);
 }
