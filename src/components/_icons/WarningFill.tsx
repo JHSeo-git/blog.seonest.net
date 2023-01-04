5eef22a9529d4e42888ad88ecb99de34304c19a7
JHSeo <qnfqnfqnf@gmail.com>
2023-01-04 23:02:00 +0900

    refactor: icons

--- src/components/_icons/WarningFill.tsx
+++ src/components/_icons/WarningFill.tsx
@@ -3,12 +3,12 @@ export type WarningFillProps = React.SVGProps<SVGSVGElement>;
 function WarningFill({ width = 24, height = 24, ...rest }: WarningFillProps) {
   return (
     <svg
+      {...rest}
       width={width}
       height={height}
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      {...rest}
     >
       <path d="M1 21H23L12 2L1 21ZM13 18H11V16H13V18ZM13 14H11V10H13V14Z" fill="currentColor" />
     </svg>
