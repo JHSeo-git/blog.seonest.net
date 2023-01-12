cfaa877d0123b59145b75be4280df12e03b7ff33
JHSeo <qnfqnfqnf@gmail.com>
2023-01-12 23:36:34 +0900

    update Blockquote dark styles

--- src/components/_mdxComponents/Blockquote.tsx
+++ src/components/_mdxComponents/Blockquote.tsx
@@ -7,7 +7,7 @@ function Blockquote({ className, ...rest }: BlockquoteProps) {
     <blockquote
       className={cn(
         'py-4 pl-5 pr-4 border-l-[5px] rounded-tr-md rounded-br-md mb-4 [&>*:last-child]:mb-0 [&>*:last-child]:text-base',
-        'bg-indigo-50  border-indigo-500 dark:bg-indigo-900',
+        'bg-indigo-50  border-indigo-500 dark:bg-gray-900 dark:border-gray-700',
         className
       )}
       {...rest}
