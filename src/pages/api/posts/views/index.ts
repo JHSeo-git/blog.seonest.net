0cc43717f740762b44a9c9cfbda18442d629fbbd
JHSeo <qnfqnfqnf@gmail.com>
2023-01-17 22:38:06 +0900

    fix: view count

--- src/pages/api/posts/views/index.ts
+++ src/pages/api/posts/views/index.ts
@@ -17,22 +17,16 @@ const postViewsIndexHandler: NextApiHandler = async (req, res) => {
     }
 
     const slug = decodeURIComponent(encodedSlug);
+    const ip = getIP(req);
+    const ipHash = createHash('sha256').update(ip).digest('hex');
+
+    await postService.viewPost({ slug, ipHash });
     const views = await postService.getViews({ slug });
 
     return res.status(200).json({
       views,
     });
   }
-
-  if (method === 'POST') {
-    const slug = decodeURIComponent(req.body.slug);
-    const ip = getIP(req);
-    const ipHash = createHash('sha256').update(ip).digest('hex');
-
-    await postService.viewPost({ slug, ipHash });
-
-    return res.status(204).end();
-  }
 };
 
-export default withCatch(withMethods(['GET', 'POST'], postViewsIndexHandler));
+export default withCatch(withMethods(['GET'], postViewsIndexHandler));
