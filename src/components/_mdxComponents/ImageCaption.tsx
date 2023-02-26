8e595196d0a23fd4b9fa858542e8b16e0c6b742e
JHSeo <qnfqnfqnf@gmail.com>
2023-02-26 17:54:41 +0900

    fix: typo

--- src/components/_mdxComponents/ImageCaption.tsx
+++ src/components/_mdxComponents/ImageCaption.tsx
@@ -8,7 +8,7 @@ function ImageCaption({ children, src, alt, ...props }: ImageCaptionProps) {
   return (
     <figure className="my-4 w-full">
       <Image {...props} src={src} alt={alt} className="my-0" />
-      <figcaption className="mt-2 text-center text-sm opacity-50">{children}</figcaption>
+      <figcaption className="mt-2 truncate text-center text-sm opacity-50">{children}</figcaption>
     </figure>
   );
 }
