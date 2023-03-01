afe04a4983946c08697e6b7d86fe90de80431f50
JHSeo <qnfqnfqnf@gmail.com>
2023-03-01 21:40:18 +0900

    feat: change pages/api to app route handlers

---
+++ src/app/api/posts/views/route.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from 'next/server';
+
+import { withRouteCatch } from '@/lib/api-middlewares/with-catch';
+import AppError from '@/lib/error';
+import * as postService from '@/services/post.service';
+
+export const GET = withRouteCatch(async (request: Request) => {
+  const url = new URL(request.url);
+
+  const encodedSlug = url.searchParams.get('slug');
+  if (!encodedSlug || typeof encodedSlug !== 'string') {
+    throw new AppError('BadRequest');
+  }
+
+  const slug = decodeURIComponent(encodedSlug);
+
+  const post = await postService.getPost({ slug });
+
+  return NextResponse.json({ views: post?.viewCount ?? 0 });
+});
+
+export const POST = withRouteCatch(async (request: Request) => {
+  const body = await request.json();
+
+  const encodedSlug = body.slug;
+  if (!encodedSlug || typeof encodedSlug !== 'string') {
+    throw new AppError('BadRequest');
+  }
+
+  const slug = decodeURIComponent(encodedSlug);
+
+  const updatedPost = await postService.viewPost({ slug });
+
+  return NextResponse.json({ views: updatedPost.viewCount });
+});
