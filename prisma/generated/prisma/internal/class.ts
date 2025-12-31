e5cd59e02b85be375dde9c87b5bf0504990dded1
JHSeo <qnfqnfqnf@gmail.com>
2025-12-31 13:48:21 +0900

    chore: update package dependencies to latest versions and adjust generated Prisma files

--- prisma/generated/prisma/internal/class.ts
+++ prisma/generated/prisma/internal/class.ts
@@ -16,8 +16,8 @@ import type * as Prisma from "./prismaNamespace.ts"
 
 const config: runtime.GetPrismaClientConfig = {
   previewFeatures: [],
-  clientVersion: "7.1.0",
-  engineVersion: "ab635e6b9d606fa5c8fb8b1a7f909c3c3c1c98ba",
+  clientVersion: "7.2.0",
+  engineVersion: "0c8ef2ce45c83248ab3df073180d5eda9e8be7a3",
   activeProvider: "postgresql",
   inlineSchema:
     '// This is your Prisma schema file,\n// learn more about it in the docs: https://pris.ly/d/prisma-schema\n\ngenerator client {\n  provider            = "prisma-client"\n  output              = "./generated/prisma"\n  importFileExtension = "ts"\n}\n\ndatasource db {\n  provider     = "postgresql"\n  relationMode = "prisma"\n}\n\nmodel Post {\n  id        Int      @id @default(autoincrement())\n  slug      String   @unique\n  viewCount Int      @default(1)\n  createdAt DateTime @default(now())\n  updatedAt DateTime @updatedAt\n}\n',
